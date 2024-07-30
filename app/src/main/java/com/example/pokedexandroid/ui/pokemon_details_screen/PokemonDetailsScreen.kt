e5a3b80db92650842ad78f35cff3cdeb0c8bd2e4
r1n1os <evagoras.frang@gmail.com>
2024-07-30 11:04:21 +0300

    start implementing UI for PokemonDetailsScreen

--- app/src/main/java/com/example/pokedexandroid/ui/pokemon_details_screen/PokemonDetailsScreen.kt
+++ app/src/main/java/com/example/pokedexandroid/ui/pokemon_details_screen/PokemonDetailsScreen.kt
@@ -1,12 +1,27 @@
 package com.example.pokedexandroid.ui.pokemon_details_screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.toRoute
+import com.example.pokedexandroid.R
 import com.example.pokedexandroid.navigations.PokemonDetailsScreen
 
 @Composable
@@ -20,7 +35,28 @@ fun PokemonDetailsScreen(
     LaunchedEffect(key1 = true) {
         pokemonDetailsViewModel.executeRequestToGetPokemonDetails(args.pokemonDetailsUrl)
     }
-    Column {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(
+                    topStart = 15.dp,
+                    topEnd = 15.dp
+                ),)
+                .padding(top = 50.dp)
+            ) {
+            Column {
+                Text(
+                    text = "Base Stats",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+        }
     }
 }
